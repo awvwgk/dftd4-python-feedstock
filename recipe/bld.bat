3e00ce3906693e7b79243b6c3393711c3161561d
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2026-05-05 17:55:06 +0200

    Update 4.1.1 (#42)
    MNT: Re-rendered with conda-smithy 3.61.2 and conda-forge-pinning 2026.05.04.16.48.4

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,5 +2,5 @@ setlocal EnableExtensions EnableDelayedExpansion
 @echo on
 
 copy assets\parameters.toml python\dftd4
-%PYTHON% -m build --outdir . --no-isolation --wheel python/
+%PYTHON% -m build --outdir . --no-isolation --wheel .
 for %%f in (*.whl) do (%PYTHON% -m pip install %%f --no-deps -vv)
